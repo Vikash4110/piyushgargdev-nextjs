c2bcc34c47265d39e5622a78b333a0d730634e2e
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2024-08-24 05:15:21 +0530

    In testimonialItem hover effect added

--- components/UI/TestimonialItem.jsx
+++ components/UI/TestimonialItem.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import classes from "../../styles/testimonial.module.css";
 
 const TestimonialItem = ({ feedBack }) => (
-  <div className={`${classes.testimonial__item}`}>
+  <div className={`${classes.testimonial__item} transform transition duration-300 ease-in-out hover:shadow-xl hover:scale-105`}>
     <div className={`${classes.testimonial__client}`}>
       <Image
         alt={feedBack.name}
@@ -14,29 +14,32 @@ const TestimonialItem = ({ feedBack }) => (
         }
         width="50"
         height="50"
-        className={`${classes.testimonial__client_Avatar} rounded-2`}
+        className={`${classes.testimonial__client_Avatar} rounded-full`}
       />
 
       <div>
-        <h6>{feedBack.name}</h6>
+        <h6 className="font-semibold text-lg">{feedBack.name}</h6>
 
         <h6>
           <Link
             aria-label={feedBack.course.title}
             target="_blank"
             href={feedBack.course.liveUrl}
+            className="text-blue-500 hover:underline"
           >
             {feedBack.course.title}
           </Link>
         </h6>
       </div>
     </div>
-    {Array(5)
-      .fill(0)
-      .map((_, index) => (
-        <i key={index} style={{ color: "#FFD700" }} className="ri-star-fill" />
-      ))}
-    <p style={{ fontSize: "20px" }}>{feedBack.content}</p>
+    <div className="flex mt-2 space-x-1">
+      {Array(5)
+        .fill(0)
+        .map((_, index) => (
+          <i key={index} style={{ color: "#FFD700" }} className="ri-star-fill" />
+        ))}
+    </div>
+    <p className="mt-2 text-gray-700 text-base">{feedBack.content}</p>
   </div>
 );
 
